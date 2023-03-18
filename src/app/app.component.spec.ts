3800ae99179b5dacf9d1e7287af31ef5504ccfbf
john-hi <john-hi@yandex.ru>
2023-03-18 19:24:48 +0300

    build: testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,19 +21,4 @@ describe('AppComponent', () => {
 
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'hermitage-history'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-
-    expect(app.title).toEqual('hermitage-history');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hermitage-history app is running!');
-  });
 });
