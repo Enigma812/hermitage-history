7eceefe107a3c8169a1ac306b8135a39410898af
Enigma812 <antiseptic812@gmail.com>
2023-03-20 14:46:54 +0300

    feat: add translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
+import { registerLocaleData } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import localeRuExtra from '@angular/common/locales/extra/ru';
+import localeRu from '@angular/common/locales/ru';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TranslateCompiler, TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { translateCompilerFactory } from './translate-compiler-factory';
+import { TranslateLoaderFactory } from './translate-loader-factory';
 
 @NgModule({
   declarations: [
@@ -11,10 +18,35 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgbModule
+    HttpClientModule,
+
+    NgbModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: TranslateLoaderFactory,
+        deps: [
+          HttpClient
+        ]
+      },
+      compiler: {
+        provide: TranslateCompiler,
+        useFactory: translateCompilerFactory
+      }
+    }),
+
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    private readonly _translate: TranslateService
+  ) {
+    registerLocaleData(localeRu, 'ru-RU', localeRuExtra);
+    this._translate.addLangs([ 'ru' ]);
+    this._translate.setDefaultLang('ru');
+    this._translate.use('ru');
+  }
+}
