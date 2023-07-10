0deed1b11070e9aa807f6a77872bb370ad517fb3
Enigma812 <antiseptic812@gmail.com>
2023-07-10 22:13:41 +0300

    feat: add building title

--- src/modules/interiors/interiors-page/interiors-page.component.ts
+++ src/modules/interiors/interiors-page/interiors-page.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Router } from '@angular/router';
 import { map, Observable, switchMap } from 'rxjs';
+import { TitleService } from 'src/app/title.service';
 
 import { DataService } from '../../../app/data.service';
 import { Interior } from '../../../app/models/interior';
@@ -12,7 +14,6 @@ import { trackBy } from '../../utils/track-by';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class InteriorsPageComponent {
-  public buildingName$: Observable<string>;
   public interiors$: Observable<Interior[]>;
 
   public trackById = trackBy('id');
@@ -20,16 +21,24 @@ export class InteriorsPageComponent {
   constructor(
     private readonly _route: ActivatedRoute,
     private readonly _dataService: DataService,
-    private readonly _router: Router
+    private readonly _router: Router,
+    private readonly _titleService: TitleService
   ) {
-    this.buildingName$ = this._route.params.pipe(map((params) => params['buildingPath']));
-
-    this.interiors$ = this._route.params.pipe(
+    const building$ = this._route.params.pipe(
       map((params) => params['buildingPath']),
       switchMap((buildingPath) => this._dataService.data$.pipe(
-        map((data) => data.buildings.find((building) => building.path === buildingPath)),
-        map((building) => building?.interiors ?? [])
+        map((data) => data.buildings.find((building) => building.path === buildingPath))
       ))
+    );
+    building$
+      .pipe(takeUntilDestroyed())
+      .subscribe((building) => {
+        if (building !== undefined) {
+          this._titleService.setTitle(building.title);
+        }
+      });
+    this.interiors$ = building$.pipe(
+      map((building) => building?.interiors ?? [])
     );
   }
 
